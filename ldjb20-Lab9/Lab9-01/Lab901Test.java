5a487f1a00b8e291eb7194a929d4f1dec3d8517e
Leon Byford <leonbyford@138-38-165-190.eduroam.bath.ac.uk>
2012-12-11 07:39:43 +0000

    [01-COMPLETE] test class

--- ldjb20-Lab9/Lab9-01/Lab901Test.java
+++ ldjb20-Lab9/Lab9-01/Lab901Test.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
+
 public class Lab901Test {
 
 	public static void main(String[] args) {
-		Triangle t = new Triangle(new double[]{1.0,2.0,3.0});
-		System.out.println(t.getPerimeter());
-		System.out.println(t.toString());
+		ArrayList<Shape> arrayShapes = new ArrayList<Shape>();
+		arrayShapes.add(new Triangle(0.8, 4.1, 4.4));
+		arrayShapes.add(new Square(2.5));
+		arrayShapes.add(new Rectangle(5.3, 0.6));
+		for (Shape shape : arrayShapes) {
+			System.out.print(shape.getClass());
+			System.out.print(" : ");
+			System.out.print(shape);
+			System.out.print(" : ");
+			System.out.println(shape.getArea());
+		}
 	}
 
 }
