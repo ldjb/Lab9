afcb6a5befcfb76e21f156ec632217af3ef8697c
Leon Byford <leonbyford@138-38-165-190.eduroam.bath.ac.uk>
2012-12-11 05:27:34 +0000

    [01] clean up Triangle.java

--- ldjb20-Lab9/Lab9-01/Triangle.java
+++ ldjb20-Lab9/Lab9-01/Triangle.java
@@ -2,8 +2,10 @@ public class Triangle extends Shape{
 
 	private double[] sides = new double[3];
 	
-	public Triangle(double[] paramSides) {
-		sides = paramSides;
+	public Triangle(double var0, double var1, double var2) {
+		sides[0] = var0;
+		sides[1] = var1;
+		sides[2] = var2;
 	}
 	
 	public double getPerimeter() {
@@ -12,6 +14,12 @@ public class Triangle extends Shape{
 	
 	public String toString() {
 		return super.toString(sides);
+	}
+	
+	public double getArea() {
+		double s = (sides[0] + sides[1] + sides[2]) * 0.5;
+		double T = Math.pow(s * (s - sides[0]) * (s - sides[1]) * (s - sides[2]), 0.5);
+		return T;
 	}
 
 }
