c687f802a921ebb0d621aa22956a642388cd69d0
Leon Byford <leonbyford@138-38-165-190.eduroam.bath.ac.uk>
2012-12-11 05:27:42 +0000

    [01] add Rectangle.java

---
+++ ldjb20-Lab9/Lab9-01/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle extends Shape{
+
+	private double[] sides = new double[4];
+	
+	public Rectangle(double var0, double var1) {
+		sides[0] = var0;
+		sides[1] = var1;
+		sides[2] = var0;
+		sides[3] = var1;
+	}
+	
+	public double getPerimeter() {
+		return super.getPerimeter(sides);
+	}
+	
+	public String toString() {
+		return super.toString(sides);
+	}
+	
+	public double getArea() {
+		return sides[0] * sides[1];
+	}
+
+}
